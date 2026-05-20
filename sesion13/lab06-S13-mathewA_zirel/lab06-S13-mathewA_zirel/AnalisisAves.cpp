2c6a2c2895b51b5e968bf985291959d6c7df54b5
Zirel-Rivera <zirel.rivera.hernandez@est.una.ac.cr>
2026-05-20 15:41:15 -0600

    AnalisisAves.cpp creacion de funciones

--- sesion13/lab06-S13-mathewA_zirel/lab06-S13-mathewA_zirel/AnalisisAves.cpp
+++ sesion13/lab06-S13-mathewA_zirel/lab06-S13-mathewA_zirel/AnalisisAves.cpp
@@ -1,1 +1,91 @@
 #include "AnalisisAves.h"
+#include <iostream>
+
+namespace EIF201 {
+	
+	NodoAve* AnalisisAves::maxAux(NodoAve* cab, NodoAve* mejor) const
+	{
+		if (cab == nullptr)
+			return mejor;
+
+		if (cab->peso > mejor->peso)
+			return nullptr;
+	}
+	int AnalisisAves::contar(NodoAve* cab) const
+	{
+		if (cab == nullptr) {
+			return;
+		}
+		return 1 + contar(cab->siguiente);
+	}
+
+	double AnalisisAves::sumaPesos(NodoAve* cab) const
+	{
+		if (cab == nullptr) {
+			return 0.0;
+		}
+		return cab->peso + sumaPesos(cab->siguiente);
+	}
+
+	int AnalisisAves::contarEspecie(NodoAve* cab, const std::string& especie) const
+	{
+		if (cab == nullptr)
+			return 0;
+
+		if (cab->especie == especie) {
+			return 1 + contarEspecie(cab->siguiente, especie);
+		}
+		else {
+			return contarEspecie(cab->siguiente, especie);
+		}
+	}
+
+	bool AnalisisAves::existeEspecie(NodoAve* cab, const std::string& especie) const
+	{
+		if (cab == nullptr)
+			return false;
+		
+		if (cab->especie == especie)
+			return true;
+
+		return existeEspecie(cab->siguiente, especie);
+	}
+
+	NodoAve* AnalisisAves::aveMasPesada(NodoAve* cab) const
+	{
+		if (cab == nullptr)
+			return nullptr;
+
+		maxAux(cab, cab);
+
+	}
+
+	void AnalisisAves::imprimirCronologico(NodoAve* cab) const
+	{
+		if (cab == nullptr)
+			return;
+
+		std::cout << cab->anillo << " "
+			<< cab->especie << " "
+			<< cab->peso << " "
+			<< cab->sexo << " "
+			<< cab->fecha << "/n ";
+
+		imprimirCronologico(cab->siguiente);
+			
+	}
+	void AnalisisAves::imprimirInversoAux(NodoAve* cab) const
+	{
+		if (cab == nullptr)
+			return;
+
+		imprimirInverso(cab->siguiente);
+
+		std::cout << cab->anillo << " "
+			<< cab->especie << " "
+			<< cab->peso << " "
+			<< cab->sexo << " "
+			<< cab->fecha << "/n ";
+	}
+
+}
--- sesion13/lab06-S13-mathewA_zirel/lab06-S13-mathewA_zirel/AnalisisAves.h
+++ sesion13/lab06-S13-mathewA_zirel/lab06-S13-mathewA_zirel/AnalisisAves.h
@@ -10,7 +10,7 @@ class AnalisisAves
 {
 private:
 	
-	void imprimirInversoAuc(NodoAve* cab) const;
+	void imprimirInversoAux(NodoAve* cab) const;
 	NodoAve* maxAux(NodoAve* cab, NodoAve* mejor) const;
 
 public:
@@ -18,7 +18,7 @@ public:
 
 	int contar(NodoAve* cab)const;
 	double sumaPesos(NodoAve* cab) const;
-	int contarEspecie(NodoAve*, const std::string& especie) const;
+	int contarEspecie(NodoAve* cab, const std::string& especie) const;
 	bool existeEspecie(NodoAve* cab, const std::string& especie) const;
 	NodoAve* aveMasPesada(NodoAve* cab) const;
 	void imprimirCronologico(NodoAve* cab) const;
