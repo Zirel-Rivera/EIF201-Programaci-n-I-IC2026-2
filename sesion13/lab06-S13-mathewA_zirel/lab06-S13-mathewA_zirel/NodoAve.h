a63e03260757d380f9942c17c4898cf2663f6dda
Mathew Araya Castillo <rafaelaraya121809@gmail.com>
2026-05-20 14:28:22 -0600

    Subida de .h
    Se suben NodoAve.h y Bitacora.h

--- sesion13/lab06-S13-mathewA_zirel/lab06-S13-mathewA_zirel/NodoAve.h
+++ sesion13/lab06-S13-mathewA_zirel/lab06-S13-mathewA_zirel/NodoAve.h
@@ -1,5 +1,24 @@
-#pragma once
-class NodoAve
+#ifndef NODO_AVE_H
+#define NODO_AVE_H
+#include<string>
+
+struct NodoAve
 {
+	std::string anillo;
+	std::string especie;
+	double peso;
+	char sexo;
+	std::string fecha;
+	NodoAve* siguiente;
+
+	NodoAve(const std::string& anillo_,
+		const std::string& especie_,
+		double peso_,
+		char sexo_,
+		std::string fecha_)
+		:anillo(anillo_),especie(especie_),peso(peso_),
+		sexo(sexo_),fecha(fecha_),siguiente(nullptr){ }
 };
+
+#endif NODO_AVE_H
 
