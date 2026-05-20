7ca7b2379fe777fb1358551d74d9e8c0941135e6
Zirel-Rivera <zirel.rivera.hernandez@est.una.ac.cr>
2026-05-20 13:59:19 -0600

    crear clase AnalisisAves.h

--- sesion13/lab06-S13-mathewA_zirel/lab06-S13-mathewA_zirel/AnalisisAves.h
+++ sesion13/lab06-S13-mathewA_zirel/lab06-S13-mathewA_zirel/AnalisisAves.h
@@ -1,5 +1,30 @@
-#pragma once
+#ifndef EIF201_ANALISIS_AVES_H
+#define EIF201_ANALISIS_AVES_H
+
+#include "NodoAve.h"
+#include <string>
+
+namespace EIF201 {
+
 class AnalisisAves
 {
+private:
+	
+	void imprimirInversoAuc(NodoAve* cab) const;
+	NodoAve* maxAux(NodoAve* cab, NodoAve* mejor) const;
+
+public:
+	AnalisisAves() = default;
+
+	int contar(NodoAve* cab)const;
+	double sumaPesos(NodoAve* cab) const;
+	int contarEspecie(NodoAve*, const std::string& especie) const;
+	bool existeEspecie(NodoAve* cab, const std::string& especie) const;
+	NodoAve* aveMasPesada(NodoAve* cab) const;
+	void imprimirCronologico(NodoAve* cab) const;
+	void imprimirInverso(NodoAve* cab) const;
 };
 
+}
+
+#endif // !1
